bf2656d7ca1c11d5101b0ea0df2dcb313c2bac21
VictorINT <victor.s.florescu@gmail.com>
2023-02-18 17:04:32 +0200

    slave now uses drive() with the values from master

--- robot_arduino_slave/robot_arduino_slave.ino
+++ robot_arduino_slave/robot_arduino_slave.ino
@@ -5,6 +5,8 @@ char buff [100];
 volatile byte index;
 volatile bool receivedone;  /* use reception complete flag */
 
+int motor_left = 0, motor_right = 0;
+
 void setup (void)
 {
   init();
@@ -16,6 +18,23 @@ void setup (void)
   SPI.attachInterrupt();    /* Attach SPI interrupt */
 }
 
+void parse()
+{
+  char mleft[5], mright[5];
+  // motor left string
+  mleft[0] = buff[0];
+  mleft[1] = buff[1];
+  mleft[2] = buff[2];
+  mleft[3] = '\0'; // terminator
+  // motor right string
+  mright[0] = buff[4];
+  mright[1] = buff[5];
+  mright[2] = buff[6];
+  mright[3] = '\0'; // terminator
+  motor_left = atoi(mleft);
+  motor_right = atoi(mright);
+}
+
 void loop (void)
 {
   if (receivedone)          /* Check and print received buffer if any */
@@ -24,6 +43,8 @@ void loop (void)
     Serial.println(buff);
     index = 0;
     receivedone = false;
+    parse();
+    drive(motor_left, motor_right);
   }
 }
 
